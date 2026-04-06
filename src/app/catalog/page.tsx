37867d7395b86b716a38504e5f717f02e817defd
sagecompsci <sagereid@gmail.com>
2026-04-06 11:32:19 -0600

    catalog list

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,23 @@
-export default function Catalog() {
-    return <p>Catalog Page</p>
+import {permanent_marker} from "@/app/ui/fonts"
+import items from '@/data/items.json'
+export default async function Catalog() {
+    return (
+        <div className="w-full">
+            <div className="text-center">
+                <h1 className={`${permanent_marker.className} text-4xl`}>Catalog</h1>
+            </div>
+
+            <div className="items-center">{items.map((item: any) => {
+                    return (
+                        <div key={item.id} className="flex items-center border-olive-900 border-solid border-4 rounded-xl m-4 p-4">
+                            <img className="mr-6 w-32 rounded-lg" src={`${item.images[0]}`} alt={`image of ${item.title}`}/>
+                            <p>{item.title}</p>
+                        </div>
+
+                    );
+                }
+            )}
+            </div>
+        </div>
+    )
 }
--- src/app/ui/side-nav.tsx
+++ src/app/ui/side-nav.tsx
@@ -4,7 +4,7 @@ import {permanent_marker} from "@/app/ui/fonts"
 
 export default function SideNav(){
     return (
-        <div className="flex h-screen w-70 flex-col px-3 py-4 md:px-2 bg-olive-500">
+        <div className="flex h-screen w-70 flex-col px-3 py-4 md:px-2 bg-olive-500 sticky top-0">
             <Link
                 href="/"
             >
